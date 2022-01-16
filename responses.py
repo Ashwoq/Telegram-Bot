e1e0f56936c91d1ad29c832a1c1fcc458ec574ac
Ashwoq <ashwoq00786@gmail.com>
2022-01-16 23:36:18 +0530

    make it better

--- responses.py
+++ responses.py
@@ -2,13 +2,22 @@ from datetime import datetime
 import random
 
 def wrong():
-    n = random.randint(0,5)
+    n = random.randint(0,8)
     if(n<=1):
         return "Moodevi Command ah Olunga type pannu!!!"
     if(n>1 and n<=2):
         return "Un Istathukku type panatha"
     if(n>2 and n<=3):
         return "Command Thappu 10thu fail ah ne?"
+    if(n>3 and n<=4):
+        return "Yeanga yen time ah waste pannanthinga COMMAND THAPPU!!!"
+    if(n>5 and n<=6):
+        return "Konjamachum Kuchathoda Vaalu Command Thappu!!!"
+    if(n>6 and n<=7):
+        return "Oru Command ah kuda correct ah poda theriyala Ne yella Yenga Pass agi .... Romba Kastam "
+    if(n>6 and n<=7):
+        return "CHI!!! Command Thappu daw"
+    
 
 
 def sample_responses(input_text,update):
@@ -32,14 +41,30 @@ def sample_responses(input_text,update):
 
     if user_msg in ('/help'):
         return """Just Die it will be helpful
-        /react - To Get the react,firebase referrence Links
-        /css - To Get the css reference Links
+    /react - To Get the react,firebase referrence Links
+    /css - To Get the css reference Links
         """
     if user_msg in ('/nakku'):
-        return "yenga"
+        return "Why are You GAY ?"
 
-    if user_msg in ('/kishore'):
-        return "Atha Reply Varathunu Theriyum la Apram yenna?"
+    if user_msg in ('/kishore' , '/kumar'):
+        return "Still Waiting for the reply!?!"
+    
+    if user_msg in ('/shameen'):
+        return "Na yarayum DEPEND Panni ila"
+    
+    if user_msg in ('/faria','/manual'):
+        return "Aambala"
+    
+    if user_msg in ('/pragha','/yeanga'):
+        return "Saptiyaa?"
+    
+    if user_msg in ('/vasan','/vasanbro','/keerthi','/keerthibro','/srk'):
+        return "Konjamachum Kuchathodu vaalu daw"
+    
+
+
+
 
     return wrong()
 
